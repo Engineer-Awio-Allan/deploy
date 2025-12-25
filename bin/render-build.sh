b60379bc4d604869e27d83f7f4ab21a0435f17d8
AWIO ALLAN <awio@kali.kai.com>
2025-12-25 20:00:50 +0300

    rvm

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,11 +3,8 @@
 # Exit immediately if any command fails
 set -o errexit
 
-# Install dependencies
+
 bundle install
-
-# Precompile Rails assets
 bundle exec rails assets:precompile
-
-# Clean up old assets
 bundle exec rails assets:clean
+bundle exec rails db:migrate
