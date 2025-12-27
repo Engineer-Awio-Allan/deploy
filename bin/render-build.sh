a4473985df061377fe0b8cb930f95891dabdb17e
AWIO ALLAN <awio@kali.kai.com>
2025-12-27 10:15:20 +0300

    Prepare for Render deployment

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
-
-# Exit immediately if any command fails
 set -o errexit
 
-
 bundle install
-bundle exec rails assets:precompile
-bundle exec rails assets:clean
 bundle exec rails db:migrate
+bundle exec rails assets:precompile
